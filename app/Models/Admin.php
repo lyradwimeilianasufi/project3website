26331d687d1bfbed287642bc35a7add235e2e25d
BJulianH <bintangjulianhidayat@gmail.com>
2025-10-22 01:40:27 +0700

    Initial commit: setup Laravel project

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
-    // Anda bisa menambahkan properti dan metode lainnya sesuai kebutuhan
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
